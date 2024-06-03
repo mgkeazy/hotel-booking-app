7e480ae952d4b4fc90e8b2c95d438ac21d36f0d0
mgkeazy <khushalgc143@gmail.com>
2024-06-03 18:13:56 +0530

    added auth and inital layouts

---
+++ backend/src/index.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import userRoutes from './routes/users';
+import authRoutes from "./routes/auth";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import mongoose from 'mongoose';
+import path from 'path';
+
+const app = express();
+app.use(cookieParser());
+
+//convert the body of api requests into json
+app.use(express.json());
+
+
+
+// by default value is undefined so we typecasted it as string
+mongoose.connect(process.env.MONGODB_CONNECT as string);
+
+
+//used to parse incoming requests with URL-encoded payloads. When extended is set to true, the qs library is used to parse the URL-encoded data, allowing for more complex data structures to be represented in the URL-encoded data.
+app.use(express.urlencoded({extended:true}));
+
+
+// for security purpose
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials:true,
+}));
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
+
+app.listen(7000,()=>{
+    console.log('Server running on PORT:7000');
+});
